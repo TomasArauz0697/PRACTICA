0658d063e5fb0449bb7ad87eac685e671b22750b
TomasArauz0697 <abdiel06arauz@gmail.com>
2021-02-21 18:01:22 -0500

    FIXED sERVER

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express=require("express");
-const path=require("path");
-const app=express();
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(express.static(__dirname+'/dist/WORMHOLE'));
-app.get('/*',function(req,res){
-res.sendFile(path.join(__dirname+'/dist/WORMHOLE/index.html'));
-});
+const app = express();
 
-app.listen(process.env.Port || 8080);
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/WORMHOLE'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/WORMHOLE/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
